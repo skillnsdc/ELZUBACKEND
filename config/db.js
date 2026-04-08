4601b5139335887658a26eb82a26f42a68e83ec1
skillindia <104757587+skillnsdc@users.noreply.github.com>
2026-04-08 21:22:31 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ei_solutions_pro', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     
